ddb3e5b5d2684a1e938940d97a71f658017a130b
Wks JanStudio <37088203+SirRina@users.noreply.github.com>
2020-08-15 13:58:28 -0300

    Gradlew added.
    I have added, so i will can start do the client.

---
+++ Rocan/build/sources/main/java/rina/rocan/mixin/RocanMixinLoader.java
@@ -0,0 +1,54 @@
+package rina.rocan.mixin;
+
+// Minecraft.
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+// Javax.
+import javax.annotation.Nullable;
+
+// Mixin utils.
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+// Java.
+import java.util.Map;
+
+/**
+  * @author Rina
+  *
+  * Created by Rina!
+  * 08/04/20.
+  *
+  **/
+public class RocanMixinLoader implements IFMLLoadingPlugin {
+	public RocanMixinLoader() {
+		MixinBootstrap.init();
+		Mixins.addConfiguration("mixins.rocan.json");
+		MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+	}
+
+	@Override
+	public String[] getASMTransformerClass() {
+		return new String[0];
+	}
+
+	@Override
+	public String getModContainerClass() {
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {}
+
+	@Override
+	public String getAccessTransformerClass() {
+		return null;
+	}
+}
